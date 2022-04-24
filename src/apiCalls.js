cbcf2b5307f92144b6796d2edb65ed754513bac0
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-04-24 11:02:53 -0400

    Allow user to successfully book room, set up post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,4 +8,23 @@ const fetchDatasets = (dataset) => {
 
 let fetchData = Promise.all([fetchDatasets('customers'), fetchDatasets('rooms'), fetchDatasets('bookings')])
 
-export { fetchData }
+const postDataset = (id, date, roomNumber) => {
+
+    return fetch('http://localhost:3001/api/v1/bookings', {
+     method: 'POST',
+     body: JSON.stringify({
+       "userID": parseInt(id),
+       "date": date,
+       "roomNumber": parseInt(roomNumber)
+     }),
+     headers: {
+       'Content-Type': 'application/json'
+     }
+   }).then(response => response.json())
+//    .then(response => console.log('post response', response))
+ 
+//    fetchData = Promise.all([fetchDatasets('bookings')])
+ }
+
+
+export { fetchData, postDataset }
